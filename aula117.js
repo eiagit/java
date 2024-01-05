208b30e5c1af48aaebec81a93f2a2d1652afaaa6
eiagit <eia_99@yahoo.com>
2024-01-04 22:37:01 -0300

    aula 117

---
+++ aula117.js
@@ -0,0 +1,17 @@
+const lat = document.querySelector("#lat");
+const lon = document.querySelector('#lon');
+const end = document.querySelector('#end');
+const btn = document.querySelector("#btn1")
+
+navigator.geolocation.getCurrentPosition(geo);
+var url = null;
+function geo(pos) {
+    lat.innerHTML=pos.coords.latitude;
+    lon.innerHTML=pos.coords.longitude;
+    url=`https://www.google.com.br/maps/@${lat.innerHTML},${lon.innerHTML},20z?entry=ttu`
+    end.innerHTML=url;
+}
+
+btn.addEventListener("click",()=>{
+    window.open(url)
+})
