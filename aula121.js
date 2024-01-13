2fb25c47b74e9d4622ecd4bb99f8d8916d2d2f68
eiagit <eia_99@yahoo.com>
2024-01-13 09:06:43 -0300

    update 121

--- aula121.js
+++ aula121.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('#btn_01')
 btn.addEventListener("click",(eve)=>{
 window.localStorage.setItem('digitado',edit.value)
 console.log(localStorage.getItem("digitado"))
+para.innerHTML=localStorage.getItem("digitado")
 })
 let num = 10;
 window.localStorage.setItem("numero",num)
+
 console.log(localStorage.getItem("numero"))
 window.localStorage.clear()
