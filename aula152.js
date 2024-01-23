1d1907f3ae6d31dc69a6e9c3cfc5222910b23342
eiagit <eia_99@yahoo.com>
2024-01-23 15:38:56 -0300

    aula 152.js

--- aula152.js
+++ aula152.js
@@ -1,4 +1,9 @@
+const dgvProdutos = document.querySelector('#dgvProdutos')
+const dgvDados = document.querySelector('#dgvDados');
+
+
 console.log("iniciei Script")
+
 const configdgv={
     endpoint :"https://veiculos.fipe.org.br",
     headers :{
@@ -18,150 +23,103 @@ const configdgv={
         body : JSON.stringify({"codigoTabelaReferencia": 231,"codigoTipoVeiculo": 1,"codigoMarca": 26})
     }
 }
-const dvg=(configdgv)=>{
-    console.log(configdgv.endpoint+configdgv.headers.path)
-    setTimeout(() => {
-    const fetchh = fetch(configdgv.endpoint+configdgv.headers.path,configdgv.headers)
-    .then(res=>res.text())
-    .then(res=>{
-        console.log(res)
-        console.log(fetchh)
-    })
-    .catch(error => console.log('error', error));
-}, 3000);
-}
+
+
 //dvg(configdgv)
 
-const code =()=> {
-/*    var myHeaders = new Headers();
-    myHeaders.append('Host', 'veiculos.fipe.org.br');
-    myHeaders.append("Referer", "https://veiculos.fipe.org.br");
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Cookie", "ROUTEID=.5");
-    myHeaders.append('Cache-Control','no-cache');
-    myHeaders.append('Content-Length',86);
-    myHeaders.append('Redirect', 'follow');
-    myHeaders.append('Path', '/api/veiculos/ConsultarModelos/');
-    var raw = JSON.stringify({ "codigoTabelaReferencia": 231, "codigoTipoVeiculo": 1, "codigoMarca": 25 });
-
-    var requestOptions = {
-        method: 'POST',
-        mode: 'no-cors',
-        path :'/api/veiculos/ConsultarModelos',
-        headers: myHeaders,
-        hostname:'veiculos.fipe.org.br',
-        body: raw,
-    };
-
-    setTimeout(() => {
-        fetch("https://veiculos.fipe.org.br"+requestOptions.path, requestOptions)
-            .then(response => response)
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
-    }, 3000);
-    
+const code = ()=>{
+ /* 
     var myHeaders = new Headers();
-    myHeaders.append("Referer", "https://veiculos.fipe.org.br");
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Cookie", "ROUTEID=.5");
-    myHeaders.append("Host", "veiculos.fipe.org.br");
-    var raw = JSON.stringify({"codigoTabelaReferencia":305,"codigoTipoVeiculo":1,"codigoMarca":25});
-    
+  myHeaders.append("Authorization", "Basic Og==");
+  
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  
+  fetch("https://v6.exchangerate-api.com/v6/813060bb2835744a51c502d5/latest/BRL", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result.conversion_rates))
+    .catch(error => console.log('error', error));
+    */
     var requestOptions = {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: myHeaders,
-      returntransfer : true,
-      post :true,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    fetch("https://veiculos.fipe.org.br/api/veiculos/ConsultarModelos", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));   
-      */
-      
-      /*
-      var myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json; charset=utf-8");
-      myHeaders.append("Cookie", "ROUTEID=.5");
-      myHeaders.append("Cache-Control","no-cache");
-      myHeaders.append("Content-Length","64");
-      myHeaders.append('Host','https://veiculos.fipe.org.br');
-      
-      var raw = JSON.stringify({"codigoTabelaReferencia": 305,"codigoTipoVeiculo": 1});
-      console.log(raw)
-      var requestOptions = {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: myHeaders,
-        body: raw,
+        method: 'GET',
         redirect: 'follow'
       };
-      
-      fetch("https://veiculos.fipe.org.br/api/veiculos/ConsultarMarcas", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-*/
-
-/*
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Cookie", "ROUTEID=.5");
-myHeaders.append("Host", "veiculos.fipe.org.br");
-
-//var raw = JSON.stringify({"codigoTabelaReferencia":215,"codigoMarca":2,"codigoTipoVeiculo":1,"anoModelo":2015,"codigoTipoCombustivel":3,"tipoVeiculo":"carro","modeloCodigoExterno":"","tipoConsulta":"tradicional","codigoModelo":4564});
-var raw = {"codigoTabelaReferencia":215,"codigoMarca":2,"codigoTipoVeiculo":1,"anoModelo":2015,"codigoTipoCombustivel":3,"tipoVeiculo":"carro","modeloCodigoExterno":"","tipoConsulta":"tradicional","codigoModelo":4564}
-
-var requestOptions = {
-  mode: 'no-cors',
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-const resposta = await fetch("http://veiculos.fipe.org.br/api/veiculos/ConsultarValorComTodosParametros", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-  console.log(resposta)
-  */
-    
-    
-const asyncPostCall = async () => {
-              try {
-                  const response = await fetch('https://veiculos.fipe.org.br/api/veiculos/consultarmodelos', {
-                        method : 'POST',
-                        mode : 'no-cors',
-                        credentials: 'same-origin',
-                        redirect : 'follow',
-                        body : JSON.stringify({"codigoTabelaReferencia": 231,"codigoTipoVeiculo": 1,"codigoMarca": 26}),
-
-                        headers : {
-                            "Content-Length" : 64,
-                            "Cache-Control" :'no-cache',
-                            Host: 'veiculos.fipe.org.br',
-                            Cookie :'ROUTEID=.5',
-                            
-                            Referer : 'https://veiculos.fipe.org.br',
-                           //"Content-Type" : "application/json"
-                        'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8 ; Access-Control-Allow-Origin = "*"'
-                        }
-                  });
-                  
-                   const data = await response;
-                // enter you logic when the fetch is successful
-                   console.log(data);
-                 } catch(error) {
-               // enter your logic for when there is an error (ex. error toast)
-                    console.log(error)
-                   } 
-              }
-  
-  asyncPostCall() 
+    const timeout = 3000
+    setTimeout(() => {
+        fetch("https://brasilapi.com.br/api/pix/v1/participants", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                result.map((dat, id) => {
+                    dgvLinhaCria(dat)
+                })
+            })
+            .catch(error => console.log('error', error));
+    }, timeout);
 }
 code()
+
+const dgvLinhaCria = (retLinha)=>{
+    const dgvLinha = document.createElement('DIV');
+    dgvLinha.setAttribute('id',retLinha.ispb)
+    dgvLinha.setAttribute('class',"dgvLinha")
+    dgvDados.appendChild(dgvLinha);
+ 
+    const dgvLinhaC1 = document.createElement('DIV');
+    dgvLinhaC1.setAttribute('id','c1'+retLinha.ispb)
+    dgvLinhaC1.setAttribute('class',"c1")
+    dgvLinhaC1.innerHTML=retLinha.ispb
+    dgvLinha.appendChild(dgvLinhaC1);
+ 
+    const dgvLinhaC2 = document.createElement('DIV');
+    dgvLinhaC2.setAttribute('id','c2'+retLinha.ispb)
+    dgvLinhaC2.setAttribute('class',"c2")
+    dgvLinhaC2.innerHTML=retLinha.nome_reduzido
+    dgvLinha.appendChild(dgvLinhaC2);    
+ 
+    const dgvLinhaC3 = document.createElement('DIV');
+    dgvLinhaC3.setAttribute('id','c3'+retLinha.ispb)
+    dgvLinhaC3.setAttribute('class',"c3")
+    dgvLinhaC3.innerHTML=retLinha.modalidade_participacao
+    dgvLinha.appendChild(dgvLinhaC3);
+ 
+    const dgvLinhaC4 = document.createElement('DIV');
+    dgvLinhaC4.setAttribute('id','c4'+retLinha.ispb)
+    dgvLinhaC4.setAttribute('class',"c4")
+    const data= new Date(retLinha.inicio_operacao)
+    const dataf =`${('0'+data.getDay()).slice(-2)}/${('0'+data.getMonth()).slice(-2)}/${('0000'+data.getFullYear()).slice(-4)}`
+    dgvLinhaC4.innerHTML= dataf
+    dgvLinha.appendChild(dgvLinhaC4);
+ 
+    const dgvLinhaC5 = document.createElement('DIV');
+    dgvLinhaC5.setAttribute('id','c5'+retLinha.ispb)
+    dgvLinhaC5.setAttribute('class',"c5")
+    
+    const dgvLinhaId = document.createElement('ion-icon');
+    dgvLinhaId.setAttribute('id','id')
+    dgvLinhaId.setAttribute('name','trash-outline')
+    dgvLinhaId.addEventListener("click",(eve)=>{
+
+    }) 
+    dgvLinhaC5.appendChild(dgvLinhaId);
+    
+    const dgvLinhaIe = document.createElement('ion-icon');
+    dgvLinhaIe.setAttribute('id','ie')
+    dgvLinhaIe.setAttribute('name','pencil-outline')
+    dgvLinhaIe.addEventListener("click",(eve)=>{
+
+    })     
+    dgvLinhaC5.appendChild(dgvLinhaIe);
+
+    const dgvLinhaIv = document.createElement('ion-icon');
+    dgvLinhaIv.setAttribute('id','Iv')
+    dgvLinhaIv.setAttribute('name','search-outline')    
+    dgvLinhaIv.addEventListener("click",(eve)=>{
+
+    }) 
+    dgvLinhaC5.appendChild(dgvLinhaIv);
+
+    dgvLinha.appendChild(dgvLinhaC5);
+}
